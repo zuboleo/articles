a2d7249ec2a1fd8027432ea7380e3d69cdc900bc
zb <zubbod@gmail.com.com>
2025-08-24 11:10:35 +0300

    Fix styles

--- src/app/core/components/header/header.ts
+++ src/app/core/components/header/header.ts
@@ -1,12 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { Button } from '@components/button/button';
 import { CurrentDate } from '@components/current-date/current-date';
 import { Line } from '@components/line/line';
 
 @Component({
   selector: 'app-header',
-  imports: [Line, CurrentDate, Button, RouterLink],
+  imports: [Line, CurrentDate],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/pages/article-list/article-list.ts
+++ src/app/pages/article-list/article-list.ts
@@ -1,12 +1,14 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { RouterLink } from '@angular/router';
+import { Button } from '@components/button/button';
 import { ArticleApiService } from '@services/article-api/article-api.service';
 import { ArticleComponent } from './components/article/article';
 import { EmptyArticleList } from './components/empty-article-list/empty-article-list';
 
 @Component({
   selector: 'app-article-list',
-  imports: [ArticleComponent, EmptyArticleList],
+  imports: [ArticleComponent, EmptyArticleList, RouterLink, Button],
   templateUrl: './article-list.html',
   styleUrl: './article-list.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
